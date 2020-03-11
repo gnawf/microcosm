5fab54ff7d9303803442391eb5b401f4a2b1856b
Franklin Wang <fractalemagic@gmail.com>
2020-03-11 21:39:28 +1100

    Fixup reader page

--- lib/ui/reader_page.dart
+++ lib/ui/reader_page.dart
@@ -2,11 +2,11 @@ import "dart:async";
 
 import "package:app/models/chapter.dart";
 import "package:app/providers/chapter_provider.dart";
-import 'package:app/resource/resource.dart';
+import "package:app/resource/resource.dart";
 import "package:app/sources/chapter_source.dart";
 import "package:app/ui/routes.dart" as routes;
 import "package:app/utils/url_launcher.dart";
-import 'package:app/widgets/mark_chapter_read.dart';
+import "package:app/widgets/mark_chapter_read.dart";
 import "package:app/widgets/settings_icon_button.dart";
 import "package:flutter/material.dart";
 import "package:flutter_hooks/flutter_hooks.dart";
